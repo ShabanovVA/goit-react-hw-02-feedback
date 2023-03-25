77787a1637a35d6253dcec4285c6322bbf5c9c1f
ShabanovVA <1514768zp@gmail.com>
2023-03-26 01:34:05 +0200

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,15 @@ export class App extends Component {
     bad: 0
   }
 
-  handleClickGood = () => {
+  handleClickGood = (e) => {
     this.setState((prevState) => ({ good: prevState.good += 1 }))
   }
   
-  handleClickNeutral = () => {
+  handleClickNeutral = (e) => {
     this.setState((prevState) => ({ neutral: prevState.neutral += 1 }))
   }
 
-  handleClickBad = () => {
+  handleClickBad = (e) => {
     this.setState((prevState) => ({ bad: prevState.bad += 1 }))
   }
 
@@ -32,16 +32,19 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h2>Please leave feedback</h2>
+        <h1>Please leave feedback</h1>
         <button type="button" onClick={this.handleClickGood}>Good</button>
         <button type="button" onClick={this.handleClickNeutral}>Neutral</button>
         <button type="button" onClick={this.handleClickBad}>Bad</button>
-        <h3>Statistics</h3>
-        <p>Good:<span>{this.state.good}</span></p>
-        <p>Neutral:<span>{this.state.neutral}</span></p>
-        <p>Bad:<span>{this.state.bad}</span></p>
-        <p>Total:<span>{this.countTotalFeedback()}</span></p>
-        <p>Positive feedback:<span>{this.countPositiveFeedbackPercentage()}%</span></p>
+        <h2>Statistics</h2>
+        {this.countTotalFeedback() > 0 ? (
+          <><p>Good:<span>{this.state.good}</span></p>
+          <p>Neutral:<span>{this.state.neutral}</span></p>
+          <p>Bad:<span>{this.state.bad}</span></p>
+          <p>Total:<span>{this.countTotalFeedback()}</span></p>
+          <p>Positive feedback:<span>{this.countPositiveFeedbackPercentage()}%</span></p>
+          </>) :
+          <h3>There is no feedback</h3>}
       </div>
     );
   }
